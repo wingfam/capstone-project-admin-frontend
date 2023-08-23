5befad7a43e49af9aeaf34f299ae4041213226d9
HoangMinhFpt <65846831+HoangMinhFpt@users.noreply.github.com>
2023-08-23 13:10:32 +0700

    update chart

--- Frontend-ReactJS-admin/src/components/Chart/LineChart.js
+++ Frontend-ReactJS-admin/src/components/Chart/LineChart.js
@@ -9,13 +9,121 @@ import { useEffect } from "react";
 import axios from "axios";
 
 Chart.register(CategoryScale);
+// function LineChart() {
+//   const intl = useIntl();
+//   const [dataChart, setDataChart] = useState([]);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         let response = await axios.get("https://localhost:44302/get-line-char");
+//         setDataChart(response.data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   var labelsChart = dataChart.map((vData) => vData.day);
+//   console.log("labelsChart", labelsChart);
+
+//   // useEffect(() => {
+//   //   var url = "https://localhost:44302/get-line-char";
+//   //   axios.get(url).then((res) => {
+//   //     setDataChart(res.data);
+//   //     console.log("Check:", dataChart);
+//   //   });
+//   // }, [dataChart]);
+//   const [lineData] = useState({
+//     if(dataChart) {
+//       labelsChart = dataChart.map((vData) => vData.day);
+//     },
+//     labels: labelsChart,
+//     datasets: [
+//       {
+//         label: intl.formatMessage({ id: "chart.orders" }),
+//         data: dataChart.map((vData) => vData.amount),
+//         backgroundColor: [
+//           "#CD853F",
+//           "#50AF95",
+//           "#f3ba2f",
+//           "#2a71d0",
+//           "#836FFF",
+//           "#00FFFF",
+//           "#8DEEEE",
+//         ],
+//         borderColor: "black",
+//         borderWidth: 2,
+//       },
+//     ],
+//   });
+//   console.log(
+//     "check:",
+//     lineData,
+//     dataChart.map((vData) => vData.day)
+//   );
+//   return (
+//     <React.Fragment>
+//       <div className="linechart-container">
+//         <div className="card">
+//           <div className="card-header">
+//             <i className="fas fa-chart-line">
+//               &nbsp; <FormattedMessage id="chart.line-chart" />
+//             </i>
+//           </div>
+//           <div className="card-body">
+//             <div className="linechart-content">
+//               <Line
+//                 data={lineData}
+//                 options={{
+//                   plugins: {
+//                     legend: {
+//                       display: false,
+//                     },
+//                     title: {
+//                       display: true,
+//                       text: intl.formatMessage({ id: "chart.week-chart" }),
+//                       font: {
+//                         size: 17,
+//                       },
+//                     },
+//                   },
+//                   scales: {
+//                     y: {
+//                       ticks: {
+//                         font: {
+//                           size: 15,
+//                         },
+//                       },
+//                     },
+//                     x: {
+//                       ticks: {
+//                         font: {
+//                           size: 15,
+//                         },
+//                       },
+//                     },
+//                   },
+//                 }}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
 function LineChart() {
   const intl = useIntl();
   const [dataChart, setDataChart] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let response = await axios.get("https://localhost:44302/get-line-char");
+        const response = await axios.get(
+          "https://localhost:44302/get-line-char"
+        );
         setDataChart(response.data);
       } catch (error) {
         console.error(error);
@@ -23,20 +131,15 @@ function LineChart() {
     };
     fetchData();
   }, []);
-  // console.log("dataChart", dataChart);
-  // useEffect(() => {
-  //   var url = "https://localhost:44302/get-line-char";
-  //   axios.get(url).then((res) => {
-  //     setDataChart(res.data);
-  //     console.log("Check:", dataChart);
-  //   });
-  // }, [dataChart]);
-  const [lineData] = useState({
-    labels: data.dataChart.map((vData) => vData.day),
+
+  const labelsChart = dataChart.map((vData) => vData.day);
+
+  const lineData = {
+    labels: labelsChart,
     datasets: [
       {
         label: intl.formatMessage({ id: "chart.orders" }),
-        data: data.dataChart.map((vData) => vData.amount),
+        data: dataChart.map((vData) => vData.amount),
         backgroundColor: [
           "#CD853F",
           "#50AF95",
@@ -50,8 +153,10 @@ function LineChart() {
         borderWidth: 2,
       },
     ],
-  });
-  console.log("check:", dataChart, data.dataChart);
+  };
+
+  console.log("check:", lineData);
+
   return (
     <React.Fragment>
       <div className="linechart-container">
